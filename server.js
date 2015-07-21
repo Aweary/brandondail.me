ceec989b3d75273fe855a7cd7b8777e2484515fe
Brandon Dail <Kierkegaurd@gmail.com>
2015-07-20 20:42:00 -0500

    Fix PORT issue

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var microbe = require('microbe.js');
 var app = microbe();
 
-app.set('publicFolder','dist');
+app.set('publicFolder', 'dist');
 
 app.route('/', function(req, res) {
   res.static('index.html');
 })
 
-app.start(3000, function(){
-  console.log('Started on port 3000');
+var port = process.env.PORT || 3000;
+
+app.start(port, function() {
+  console.log('Started on port %j', port);
 })
